a7aca4dcaccf41a8050d8c3489214d7ec1b43fcf
dimaschronicles <dimasmadridista834@gmail.com>
2020-08-22 09:32:51 +0700

    restapi - pert2 - (membuat web sederhana dengan file json pada php dan javascript)

---
+++ wpu-hut/js/script.js
@@ -0,0 +1,45 @@
+function tampilkanSemuaMenu() {
+  $.getJSON('data/pizza.json', function (data) {
+    // console.log(data);
+
+    let menu = data.menu; // menghilangkan key menu dari file.json
+
+    $.each(menu, function (i, data) {
+      $('#daftar-menu').append('<div class="col-md-4"><div class= "card mb-3" ><img src="img/menu/' + data.gambar + '"class= "card-img-top" ><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5 class="card-title">Rp. ' + data.harga + ' ,-</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div ></div >');
+    });
+
+  });
+}
+
+tampilkanSemuaMenu();
+
+
+$('.nav-link').on('click', function () {
+  // mengubah warna tombol ketika diklik
+  $('.nav-link').removeClass('active');
+  $(this).addClass('active');
+
+  // mengubah tulisan sesuai menu
+  let kategori = $(this).html();
+  $('h1').html(kategori);
+
+  // menampilkan semua data
+  if (kategori == 'All Menu') {
+    tampilkanSemuaMenu();
+    return;
+  }
+
+  // menampilkan data sesuai menu / kategori
+  $.getJSON('data/pizza.json', function (data) {
+    let menu = data.menu;
+    let content = '';
+
+    $.each(menu, function (i, data) {
+      if (data.kategori == kategori.toLowerCase()) {
+        content += '<div class="col-md-4"><div class= "card mb-3" ><img src="img/menu/' + data.gambar + '"class= "card-img-top" ><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5 class="card-title">Rp. ' + data.harga + ' ,-</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div ></div >';
+      }
+    });
+
+    $('#daftar-menu').html(content);
+  });
+});
